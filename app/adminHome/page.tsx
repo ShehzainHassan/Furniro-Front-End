ff777125c69fcf91d765c10d126be9b02008be82
RaffaySajjad <raffaysajjad.apple@gmail.com>
2024-12-16 14:15:46 +0500

    Added Icons in Admin Dashboard Page

--- app/adminHome/page.tsx
+++ app/adminHome/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { FaEdit } from "react-icons/fa";
+import { ImStatsBars } from "react-icons/im";
+import { IoMdAddCircle } from "react-icons/io";
+import { MdDelete, MdManageHistory } from "react-icons/md";
 import classes from "./adminDashboard.module.css";
-
 const AdminDashboard = () => {
   const [isDropdownVisible, setDropdownVisible] = useState(false);
   const router = useRouter();
@@ -27,31 +30,47 @@ const AdminDashboard = () => {
     <div className={classes.dashboardContainer}>
       <h1 className={classes.title}>Admin Home Page</h1>
       <div className={classes.buttonsContainer}>
-        <button className={classes.button} onClick={toggleDropdown}>
-          Manage Products
-        </button>
+        <div className={classes.subBtnContainer}>
+          <MdManageHistory />
+          <button className={classes.button} onClick={toggleDropdown}>
+            Manage Products
+          </button>
+        </div>
         {isDropdownVisible && (
           <div className={classes.dropdown}>
-            <button
-              onClick={handleAddProduct}
-              className={classes.dropdownButton}>
-              Add Product
-            </button>
-            <button
-              onClick={handleEditProduct}
-              className={classes.dropdownButton}>
-              Edit Product
-            </button>
-            <button
-              onClick={handleDeleteProduct}
-              className={classes.dropdownButton}>
-              Delete Product
-            </button>
+            <div className={classes.subBtnContainer}>
+              <IoMdAddCircle />
+              <button
+                onClick={handleAddProduct}
+                className={classes.dropdownButton}>
+                Add Product
+              </button>
+            </div>
+            <div className={classes.subBtnContainer}>
+              <FaEdit />
+              <button
+                onClick={handleEditProduct}
+                className={classes.dropdownButton}>
+                Edit Product
+              </button>
+            </div>
+
+            <div className={classes.subBtnContainer}>
+              <MdDelete />
+              <button
+                onClick={handleDeleteProduct}
+                className={classes.dropdownButton}>
+                Delete Product
+              </button>
+            </div>
           </div>
         )}
-        <button onClick={handleViewStatistics} className={classes.button}>
-          View Statistics
-        </button>
+        <div className={classes.subBtnContainer}>
+          <ImStatsBars />
+          <button onClick={handleViewStatistics} className={classes.button}>
+            View Statistics
+          </button>
+        </div>
       </div>
     </div>
   );
