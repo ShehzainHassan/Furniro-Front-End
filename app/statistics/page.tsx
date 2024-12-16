7187dfbe05597d30344f1115fd28efa44e668a34
RaffaySajjad <raffaysajjad.apple@gmail.com>
2024-12-16 12:19:41 +0500

    Added product category and user types piechart for admin statistics

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,27 +1,27 @@
 "use client";
 import axios from "axios";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 import { useEffect, useState } from "react";
 import classes from "./page.module.css";
-import HighchartsReact from "highcharts-react-official";
-import Highcharts from "highcharts";
 
-const options = {
-  series: [
-    {
-      data: [
-        { name: "Chair", y: 25 },
-        { name: "Sofa", y: 20 },
-        { name: "Table", y: 11 },
-        { name: "Decor", y: 5 },
-      ],
-    },
-  ],
-};
 export default function Statistics() {
+  interface CategoryData {
+    name: string;
+    y: number;
+  }
+  interface UserCategory {
+    category: "USER" | "ADMIN";
+  }
+
+  interface ProductCategory {
+    category: "Chair" | "Sofa" | "Table" | "Decor";
+  }
   const [userCount, setUserCount] = useState<number>(0);
   const [productCount, setProductCount] = useState<number>(0);
   const [regularUsers, setRegularUserCount] = useState<number>(0);
-
+  const [productCategory, setProductCategory] = useState<CategoryData[]>([]);
+  const [userCategory, setUserCategory] = useState<CategoryData[]>([]);
   const loadUserCount = async () => {
     try {
       const response = await axios.get(
@@ -30,9 +30,20 @@ export default function Statistics() {
           headers: { Authorization: `Bearer ${token}` },
         }
       );
-
       setUserCount(response.data.length);
       setRegularUserCount(response.data.length - 1);
+      setUserCategory(response.data.length);
+      const categoryCount: { [key: string]: number } = {};
+
+      response.data.forEach((user: UserCategory) => {
+        const category = user.category;
+        categoryCount[category] = (categoryCount[category] || 0) + 1;
+      });
+
+      setUserCategory([
+        { name: "USER", y: categoryCount["USER"] || 0 },
+        { name: "ADMIN", y: categoryCount["ADMIN"] || 0 },
+      ]);
     } catch (err) {
       console.error("Error loading users", err);
     }
@@ -47,6 +58,19 @@ export default function Statistics() {
       );
 
       setProductCount(response.data.length);
+      const categoryCount: { [key: string]: number } = {};
+
+      response.data.forEach((product: ProductCategory) => {
+        const category = product.category;
+        categoryCount[category] = (categoryCount[category] || 0) + 1;
+      });
+
+      setProductCategory([
+        { name: "Chair", y: categoryCount["Chair"] || 0 },
+        { name: "Sofa", y: categoryCount["Sofa"] || 0 },
+        { name: "Table", y: categoryCount["Table"] || 0 },
+        { name: "Decor", y: categoryCount["Decor"] || 0 },
+      ]);
     } catch (err) {
       console.error("Error loading users", err);
     }
@@ -58,6 +82,29 @@ export default function Statistics() {
   if (typeof window == "undefined") return null;
   const token = localStorage.getItem("JWT");
 
+  const productOptions = {
+    chart: {
+      type: "pie",
+    },
+    series: [
+      {
+        type: "pie",
+        data: productCategory,
+      },
+    ],
+  };
+
+  const userOptions = {
+    chart: {
+      type: "pie",
+    },
+    series: [
+      {
+        type: "pie",
+        data: userCategory,
+      },
+    ],
+  };
   return (
     <div className={classes.container}>
       <h1 className={classes.title}>Statistics</h1>
@@ -75,13 +122,16 @@ export default function Statistics() {
               <p>Admin User</p>
             </div>
           </div>
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={userOptions}></HighchartsReact>
         </div>
         <div className={classes.statCard}>
           <p className={classes.statLabel}>Total Products</p>
           <p className={classes.statValue}>{productCount}</p>
           <HighchartsReact
             highcharts={Highcharts}
-            options={options}></HighchartsReact>
+            options={productOptions}></HighchartsReact>
         </div>
       </div>
     </div>
