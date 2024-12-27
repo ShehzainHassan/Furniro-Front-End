a301339d566b902dac53aadcc4fbd44e93b8afcb
ShehzainHassan <shehzainhassan@gmail.com>
2024-12-27 15:13:53 +0500

    Fixed redirection bug

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import { NextResponse } from "next/dist/server/web/spec-extension/response";
-
+const URL = process.env.FRONT_END_URL;
 export function middleware(req) {
   const token = req.cookies.get("JWT");
   let decodedToken = null;
@@ -35,14 +35,15 @@ export function middleware(req) {
     (adminOnlyRoutes.some((route) => url.includes(route)) ||
       userRoutes.some((route) => url.includes(route)))
   ) {
-    return NextResponse.redirect("https://furniro.up.railway.app//");
+    return NextResponse.redirect(`${URL}`);
   }
   if (token) {
     if (
       adminOnlyRoutes.some((route) => url.includes(route)) &&
       !decodedToken?.role.includes("ADMIN")
     ) {
-      return NextResponse.redirect("https://furniro.up.railway.app/home");
+      console.log("Admin Route = ", url);
+      return NextResponse.redirect(`${URL}/home`);
     }
   }
 
